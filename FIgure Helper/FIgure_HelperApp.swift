e7cf1eb3c51231bbf5c4fbbd6287d69085fb9350
Anthony Jin <ajin@lcs.0n.ca>
2021-12-01 09:53:32 -0500

    Initial Commit

---
+++ FIgure Helper/FIgure_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIgure_HelperApp.swift
+//  FIgure Helper
+//
+//  Created by Anthony Jin on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct FIgure_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
